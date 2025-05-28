75d5bf73e52bf8104ad5607eef155a7aa1bf37a1
Ethan Wang <avaniawang@outlook.com>
2025-05-28 10:02:32 +0800

    add web service

--- internal/web/gin.go
+++ internal/web/gin.go
@@ -1,1 +1,35 @@
 package web
+
+import (
+	"net/http"
+
+	"github.com/chanmaoganda/fileshare/internal/model"
+	"github.com/chanmaoganda/fileshare/internal/pkg/dbmanager"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type WebService struct {
+	*gin.Engine
+	Manager *dbmanager.DBManager
+}
+
+func NewWebService(DB *gorm.DB) *WebService {
+	service := &WebService{
+		Manager: dbmanager.NewDBManager(DB),
+		Engine:  gin.Default(),
+	}
+	service.RegisterRoutes()
+	return service
+}
+
+func (s *WebService) RegisterRoutes() {
+	s.GET("/fileinfo", s.GetFileInfo)
+}
+
+func (s *WebService) GetFileInfo(c *gin.Context) {
+	var fileInfos []model.FileInfo
+	s.Manager.DB.Find(&fileInfos)
+
+	c.JSON(http.StatusOK, gin.H{"data": fileInfos})
+}
