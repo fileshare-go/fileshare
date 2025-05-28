c6e8493781bff75cf7338494afae2e1deb05ac12
Ethan Wang <avaniawang@outlook.com>
2025-05-28 17:28:25 +0800

    simple web service

--- internal/web/gin.go
+++ internal/web/gin.go
@@ -24,12 +24,28 @@ func NewWebService(DB *gorm.DB) *WebService {
 }
 
 func (s *WebService) RegisterRoutes() {
-	s.GET("/fileinfo", s.GetFileInfo)
+	s.GET("/fileinfo", s.GetFileInfos)
+	s.GET("/sharelink", s.GetShareLinks)
+	s.GET("/record", s.GetRecords)
 }
 
-func (s *WebService) GetFileInfo(c *gin.Context) {
+func (s *WebService) GetFileInfos(c *gin.Context) {
 	var fileInfos []model.FileInfo
 	s.Manager.DB.Find(&fileInfos)
 
 	c.JSON(http.StatusOK, gin.H{"data": fileInfos})
+}
+
+func (s *WebService) GetShareLinks(c *gin.Context) {
+	var shareLinks []model.ShareLink
+	s.Manager.DB.Find(&shareLinks)
+
+	c.JSON(http.StatusOK, gin.H{"data": shareLinks})
+}
+
+func (s *WebService) GetRecords(c *gin.Context) {
+	var records []model.Record
+	s.Manager.DB.Find(&records)
+
+	c.JSON(http.StatusOK, gin.H{"data": records})
 }
