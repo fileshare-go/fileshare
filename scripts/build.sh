989c3cf9e6299b5bbde4df2a641219e10ea07851
Ethan Wang <avaniawang@outlook.com>
2025-06-04 23:16:51 +0800

    add build script to build docker images

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,12 +13,23 @@ mkdir -p bin
 
 # linux
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o bin/fileshare .
-tar -cvf "fileshare-$TAG-linux.tar.gz" fileshare
+tar -cvf "fileshare-$TAG-linux.tar.gz" bin/fileshare
 
 # windows
 export CC=x86_64-w64-mingw32-gcc
 export CXX=x86_64-w64-mingw32-g++
 CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o bin/fileshare.exe .
-tar -cvf "fileshare-$TAG-windows.tar.gz" fileshare.exe
+tar -cvf "fileshare-$TAG-windows.tar.gz" bin/fileshare.exe
+
+unset CC
+unset CXX
 
 rm bin/fileshare bin/fileshare.exe
+
+echo "Building docker image"
+
+docker build -t fileshare:$TAG .
+
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+
+docker save fileshare:$TAG > fileshare-$TAG.docker.zip
